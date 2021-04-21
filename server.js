3a1a861d83fcaed2fa8cdb49fe52cb33fe87ecda
HaiderRizvii <sha.rizvi883@gmail.com>
2021-04-21 05:24:14 +0500

    first route added

--- server.js
+++ server.js
@@ -2,13 +2,24 @@ const express = require('express');
 const dotenv = require('dotenv');
 
 //Load Env to use .env file variables.
-
 dotenv.config({path: './config/config.env'});
 
 const app = express();
 
+//Routes 
+app.get('/', (req, res) =>{
+    res.status(200)
+        .send('<h1>Welcome to DevBootCamps!!</h1>');       
+});
+
+//.send('Welcome to DevBootCamps!!');
+//.send({ data: 'Welcome to DevBootCamps!!'});
+//We can sent JSON by simple passing JS object.
+
+//PORT and lesion server 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(
+    PORT, () => {
     console.log(`App listening on port ${PORT} in ${process.env.NODE_ENV} mode!!`);
 });
